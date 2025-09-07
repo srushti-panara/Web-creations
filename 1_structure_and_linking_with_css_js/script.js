9d1188309da6983778b4c080180611203821d938
Srushti Panara <srushtipanara1737@gmail.com>
2025-09-07 21:20:06 +0530

    Add files via upload

---
+++ 1_structure_and_linking_with_css_js/script.js
@@ -0,0 +1,9 @@
+alert ( " Welcome to Web Dev Course! ")
+
+// alert : used to show popup message in browser when page loads 
+// " " : used to write string message
+
+// single line comment
+/* 
+multi line comment
+*/
